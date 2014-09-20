e5cd10beec001f3f4b3a90811dce1bb44bcca081
sauto <nositohi@gmail.com>
2014-09-20 15:07:42 +0900

    アニメリスト

---
+++ test1/test1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RestTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Check,
+            this.Title,
+            this.Time});
+            this.dataGridView1.Location = new System.Drawing.Point(32, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(348, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "更新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RestTime
+            // 
+            this.RestTime.Location = new System.Drawing.Point(120, 295);
+            this.RestTime.Name = "RestTime";
+            this.RestTime.ReadOnly = true;
+            this.RestTime.Size = new System.Drawing.Size(100, 19);
+            this.RestTime.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "残り時間";
+            // 
+            // Check
+            // 
+            this.Check.HeaderText = "";
+            this.Check.Name = "Check";
+            this.Check.Width = 25;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "タイトル";
+            this.Title.Name = "Title";
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "時間(分)";
+            this.Time.Name = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 448);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RestTime);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox RestTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Check;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+
+    }
+}
+
---
+++ test1/test1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            int summary = 0;
+            int result;
+            string fileName = System.Windows.Forms.Application.StartupPath + "\\" + "data.xml";
+
+            //データファイルがない場合DL
+            //System.Net.WebClient wc = new System.Net.WebClient();
+            //wc.DownloadFile("http://blog-imgs-60.fc2.com/t/o/t/tottirakatta/XMLFile1.xml", fileName);
+            //wc.Dispose();
+
+            
+            //データファイルからデータを取得
+            //XmlSerializerオブジェクトを作成
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(Datas));
+            //読み込むファイルを開く
+            System.IO.StreamReader sr = new System.IO.StreamReader(
+                fileName, System.Text.Encoding.GetEncoding("shift_jis"));
+            //XMLファイルから読み込み、逆シリアル化する
+            Datas dataList = (Datas)serializer.Deserialize(sr);
+            //ファイルを閉じる
+            sr.Close();
+
+            //取得データから表の生成
+            foreach(Data data in dataList.datas)
+            {
+                this.dataGridView1.Rows.Add(false, data.Title, data.Time);
+                int.TryParse(data.Time, out result);
+                summary += result;
+            }
+
+
+            //チェックなどのデータを保存
+            
+
+            //残り時間の表示
+            RestTime.Text = (summary/60).ToString() + "時間" + (summary%60).ToString() + "分";
+        }
+
+        [System.Xml.Serialization.XmlRoot("anime")]
+        public class Datas
+        {
+            /// <summary>
+            /// データ
+            /// </summary>
+            [System.Xml.Serialization.XmlElement("data")]
+            public System.Collections.Generic.List<Data> datas { get; set; }
+        }
+
+        public class Data
+        {
+            /// <summary>
+            /// ID
+            /// </summary>
+            [System.Xml.Serialization.XmlAttribute("id")]
+            public String ID { get; set; }
+            /// <summary>
+            /// 名前
+            /// </summary>
+            [System.Xml.Serialization.XmlElement("title")]
+            public String Title { get; set; }
+            /// <summary>
+            /// 苗字
+            /// </summary>
+            [System.Xml.Serialization.XmlElement("time")]
+            public String Time { get; set; }
+        }
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DLしてきたやつからデータ取得
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //チェックしたら時間を0にする
+
+            if (e.ColumnIndex == this.dataGridView1.Columns["Check"].Index)
+            {
+
+                this.dataGridView1[this.dataGridView1.Columns["Time"].Index, e.RowIndex].Value = "0";
+            }
+
+            int summary = 0;
+            int result;
+            foreach(var row in this.dataGridView1.Rows)
+            {
+                //int.TryParse(row, out result);
+                //summary += result;
+            }
+
+            //残り時間の表示
+            RestTime.Text = (summary / 60).ToString() + "時間" + (summary % 60).ToString() + "分";
+        }
+
+
+
+
+    }
+}
+/*設計
+ * アニメのリストをDLしてきて表示する
+ * 設定によって自前も可能
+ * リストのファイル形式はxml
+ * 見終わったらチェックボックスをONにする
+ * チェックONのアニメは視聴時間が0になる
+ * 視聴時間の合計を表示する
+ */
+
+
+
